c8359ea0608ac3a0d5200fb96f604f6bfa8e1cbd
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-02-26 11:32:43 -0500

    Creates InterviewerList component and refactors controlled lists to value / onChange

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,13 +9,13 @@ export default function InterviewerListItem(props) {
   })
 
   return (
-    <li className={interviewerItemClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerItemClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected && <>{props.name}</>}
+      {props.selected && props.name}
     </li>
   );
 }
