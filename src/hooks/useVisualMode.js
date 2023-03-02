07fc549ef7bbb8ffc479c9eb6259adce427ec885
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-01 17:56:45 -0800

    Creates custom useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+
+    setMode(mode);
+    setHistory([...history, mode]);
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length-1]);
+    }
+  }
+
+  return { 
+    mode, 
+    transition,
+    back 
+  };
+  
+}
