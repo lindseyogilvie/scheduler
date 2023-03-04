eab5883ada25e5a959890c96b2af754e958144c9
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-03 17:29:22 -0800

    Creates tests for Application component

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -96,7 +96,7 @@ export default function useApplicationData() {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
-      axios.get('api/interviewers')
+      axios.get('/api/interviewers')
     ]).then((all) => {
       console.log(all);
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
